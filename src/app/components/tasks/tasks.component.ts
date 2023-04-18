0c1956f7d3a1b72c339d03528cc4e92e7109cab5
Aya Hamza <117121490+aaiaa-1@users.noreply.github.com>
2023-04-18 14:16:45 +0000

    json-server, HttpClient
    json-server for the fake api backend and HttpClient for fetching data from the json-server (get method)

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from '../../services/task.service';  
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
 
-}
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService){}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(
+      (tasks)=>this.tasks = tasks
+    );
+  }
+}  
