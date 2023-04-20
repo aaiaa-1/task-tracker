514a8f7ec5805803c638747c485d341eee1945f8
Aya Hamza <117121490+aaiaa-1@users.noreply.github.com>
2023-04-20 16:17:25 +0000

    on toggle add task
    show the add task section only if we click the add button

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-add-task',
@@ -13,7 +16,17 @@ export class AddTaskComponent implements OnInit {
   day!: string;
   reminder: boolean = false;
 
-  constructor(){ }
+  showAddTask: boolean = false;
+  subcription!: Subscription;
+
+
+  constructor(private uiService: UiService){
+    this.subcription = this.uiService
+                                    .onToggle()
+                                    .subscribe(
+                                      (value)=> (this.showAddTask = value) 
+                                    ); 
+  }
    
   ngOnInit(): void {  }
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,11 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker'; 
+  showAddTask!: boolean;
+  subcription!: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subcription = this.uiService
+                                    .onToggle()
+                                    .subscribe(
+                                      (value)=> (this.showAddTask = value) 
+                                    );  
+  }
 
   ngOnInit(): void {}
   toggleAddTask(){
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
